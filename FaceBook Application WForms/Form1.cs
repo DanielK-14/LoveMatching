a4336454c79e95bfc69c7a0ef96441633b67f838
DanielK-14 <dany,kalmikov@gmail.com>
2020-08-02 21:59:07 +0300

    Add project files.

---
+++ FaceBook Application WForms/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FaceBook_Application_WForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
